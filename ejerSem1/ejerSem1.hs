c19dd1003d695739fab6c6282cba41b55cca9804
Jess <jess@Persephone0.(none)>
2016-02-19 00:29:55 -0600

    casos base algunas funciones

--- ejerSem1/ejerSem1.hs
+++ ejerSem1/ejerSem1.hs
@@ -16,56 +16,62 @@ data L = El | ConctP L L deriving Show
 
 --Concatena dos cadenas de L.
 conctL :: L->L->L
+conctL El El = El
+conctL El l = l
+conctL l El = l
 
 --Convierte cadenas de M en L.
-mToL :: M->L
+--mToL :: M->L
+--mTol Em = El
 
 
 --Convierte cadenas de L en M.
 lToM :: L->M
+lToM El = Em
 
 
-
---Tipo de dato para implementar el lexer.
-data Tokens = ParA | ParC | Desc deriving (Show, Eq)
-
-
---Lexer que recibe una cadena de texto y lo convierte en tokens.
-lexer :: String->[Tokens]
+----Tipo de dato para implementar el lexer.
+--data Tokens = ParA | ParC | Desc deriving (Show, Eq)
 
 
---Tipo de dato para representar los juicios de análisis sintáctico.
-data Pila = Pila Int [Tokens] 
-
---Función que hace un análisis sintáctico para determinar si una cadena está balanceada o no.
-analiSintc :: Pila->Bool
+----Lexer que recibe una cadena de texto y lo convierte en tokens.
+--lexer :: String->[Tokens]
 
 
---Función que determina si una cadena está formada por paréntesis y está balanceada.
-esBalanceada :: String->Bool
+----Tipo de dato para representar los juicios de análisis sintáctico.
+--data Pila = Pila Int [Tokens] 
 
---Función que convierte una cadena de texto balanceada en un objeto de tipo M.
+----Función que hace un análisis sintáctico para determinar si una cadena está balanceada o no.
+--analiSintc :: Pila->Bool
+
+
+----Función que determina si una cadena está formada por paréntesis y está balanceada.
+--esBalanceada :: String->Bool
+
+----Función que convierte una cadena de texto balanceada en un objeto de tipo M.
 parserM :: String->M
+parserM "" = Em
 
 
---Función que convierte una cadena de texto balanceada en un objeto de tipo L.
+----Función que convierte una cadena de texto balanceada en un objeto de tipo L.
 parserL :: String->L
+parserL "" = El
 
 
---PRUEBAS:
-prueba1 = show (Par $ ConctM (Par $ Par Em) (Par Em)) == "((())())"
-prueba2 = show (ConctP (ConctP (ConctP El El) (ConctP El El)) El)  == "((())())" 
-prueba3 = show (conctL (parserL "()()()") (parserL "(())")) == "()()()(())"  
-prueba4 = show (mToL $ parserM "(())()(())") == "(())()(())" 
-prueba5 = show (lToM $ parserL "(())()(())()") == "(())()(())()"
-prueba6 = lexer (show $ parserM "(()((())(())))") == 
-                              [ParA,ParA,ParC,ParA,ParA,ParA,ParC,ParC,ParA,ParA,ParC,ParC,ParC,ParC]
-prueba7 = lexer (show $ parserL "(()((())(())))") == 
-                              [ParA,ParA,ParC,ParA,ParA,ParA,ParC,ParC,ParA,ParA,ParC,ParC,ParC,ParC]
-prueba8 = analiSintc $ Pila 0 $ lexer "(()((())(())))"
---Sólo estas pruebas deben devolver False.
-prueba9 = analiSintc $ Pila 0 $ lexer "(()((())(()))"
-prueba10 = analiSintc $ Pila 0 $ lexer "(()((())())))"
+----PRUEBAS:
+--prueba1 = show (Par $ ConctM (Par $ Par Em) (Par Em)) == "((())())"
+--prueba2 = show (ConctP (ConctP (ConctP El El) (ConctP El El)) El)  == "((())())" 
+--prueba3 = show (conctL (parserL "()()()") (parserL "(())")) == "()()()(())"  
+--prueba4 = show (mToL $ parserM "(())()(())") == "(())()(())" 
+--prueba5 = show (lToM $ parserL "(())()(())()") == "(())()(())()"
+--prueba6 = lexer (show $ parserM "(()((())(())))") == 
+--                              [ParA,ParA,ParC,ParA,ParA,ParA,ParC,ParC,ParA,ParA,ParC,ParC,ParC,ParC]
+--prueba7 = lexer (show $ parserL "(()((())(())))") == 
+--                              [ParA,ParA,ParC,ParA,ParA,ParA,ParC,ParC,ParA,ParA,ParC,ParC,ParC,ParC]
+--prueba8 = analiSintc $ Pila 0 $ lexer "(()((())(())))"
+----Sólo estas pruebas deben devolver False.
+--prueba9 = analiSintc $ Pila 0 $ lexer "(()((())(()))"
+--prueba10 = analiSintc $ Pila 0 $ lexer "(()((())())))"
 
 
 
