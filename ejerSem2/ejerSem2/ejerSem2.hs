9d6c440189944f4685ccb5e4b219107b5b8f73a5
Jess <jess@Persephone0.(none)>
2016-02-27 13:23:49 -0600

    All working but pruebar2

--- ejerSem2/ejerSem2/ejerSem2.hs
+++ ejerSem2/ejerSem2/ejerSem2.hs
@@ -18,7 +18,9 @@ sust::Exp->String->Exp->Exp
 sust (Num a) varSust exp1 = Num a
 sust (Var c) varSust exp1 = if (varSust == c) then exp1 else Var c
 sust (Suma exp1 exp2) varSust exp3 = Suma (sust exp1 varSust exp3) (sust exp2 varSust exp3)
-sust (Let varSust exp1 exp2) varSust2 exp3 = Let varSust (sust exp1 varSust2 exp3) (sust exp2 varSust2 exp3)
+sust (Let varSust exp1 exp2) varSust2 exp3 = if varSust `elem` (varSust2:(freeVars exp3)) 
+																						then (Let varSust exp1 exp2) 
+																						else (Let varSust (sust exp1 varSust2 exp3) (sust exp2 varSust2 exp3))
 
 eval::Exp->Int
 eval (Num a) = a
@@ -36,25 +38,30 @@ prueba1 = eval $ Let "x" (Num 3) $ Let "y" (Num 7) $ Suma (Var "x") (Suma (Num 4
 -- (let x = (let z = 5 in z+3 end) in x end) + (let y = 7 in 5+y end)
 prueba2 = eval $ Suma (Let "x" (Let "z" (Num 5) (Suma (Var "z") (Num 3))) (Var "x")) (Let "y" (Num 7) (Suma (Num 5) (Var "y")))
 
--- Debe de dar 60.
+-- Debe dar 60.
 -- let x = (let y = 3 in (14+y)+5 end) in let z = x+16 in z+x end end
 prueba3 = eval $ Let "x" (Let "y" (Num 3) (Suma (Suma (Num 14) (Var "y")) (Num 5))) (Let "z" (Suma (Var "x") (Num 16)) (Suma (Var "z") (Var "x")))
 
--- Debe de dar 221.
+-- Debe dar 221.
 -- let x = 3 + (let y = 98 in 98 + (1+y) end) in let y = 19 in x + (let z = y in z+2 end) end end
+prueba4 = eval $ Let "x" (Suma (Num 3) (Let "y" (Num 98) (Suma (Num 98) (Suma (Num 1) (Var "y")))))               
+                (Let "y" (Num 19) (Suma (Var "x") (Let "z" (Var "y") (Suma (Var "z") (Num 2) ))))
 
---prueba4 =  eval $ Let "x" (Suma (Num 3) (Let "y" (Num 98) (Suma (Num 98) (Suma (Num 1) (Var "y"))))) (Let "y" (Num 19) (Suma (Var "x") (Let "z" (Var "y") (Suma (Var "z") (Num 2)))))
---prueba4 =  eval $ Let "x" (Suma (Num 3) (Let "y" (Num 98) (Suma (Num 98) (Suma (Num 1) (Var "y"))))) (Let "y" (Num 19) (Suma (Var "x") (Let "z" (Var "y") (Suma (Var "z") (Num 2)))))
-prueba4 = eval $ Let "x" (Suma (Num 3) (Let "y" (Num 98) (Suma (Num 98) (Suma (Num 1) (Var "y"))))) (Let "y" (Num 19) ( Suma (Var "x") (Let "z" (Var "y") (Suma (Var "z") (Num 2) ))))
 
+-- Debe dar (221, 5)
+prueba5 = evalreto (Let "x" (Suma (Num 3) (Let "y" (Num 98) (Suma (Num 98) (Suma (Num 1) (Var "y"))))) (Let "y" (Num 19) (Suma (Var "x") (Let "z" (Var "y") (Suma (Var "z") (Num 2) ))))) 0
 
 {-RETO EXTRA-}
 evalreto::Exp->Int->(Int,Int)
-evalreto = error "Te toca"
---evalreto (Num a) 0 = (a, 0)
---evalreto (Var c) 0 = error "NOPE"
---evalreto (Suma exp1 exp2) n = (eval(exp1) + eval(exp2), n+1) usar let (v1,n1)
---evalreto (Let varSust exp1 exp2) = eval (sust exp2 varSust exp1)
+evalreto (Num a) _ = (a, 0)
+evalreto (Var c) n = error "NOPE"
+evalreto (Suma exp1 exp2) n = (x1+x2,y1+y2+1) where
+															(x1,y1) = (evalreto exp1 0)
+															(x2,y2) = (evalreto exp2 0) 
+evalreto (Let varSust exp1 exp2) n = evalreto (sust exp2 varSust exp1) (y1+y2+y3)
+																		where (x1,y1) = evalreto exp1 0
+																		      (x2,y2) = evalreto exp2 0
+																		      (x3,y3) = evalreto (sust exp2 varSust exp1) (y1+y2)
 
 
 {-PRUEBAS RETO-}
