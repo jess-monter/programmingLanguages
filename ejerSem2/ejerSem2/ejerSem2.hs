4ec00c806e5aff8fc37436d671cdcdf0fe7ee6b6
jess <jess@debian>
2016-03-01 19:05:10 +0000

    done

--- ejerSem2/ejerSem2/ejerSem2.hs
+++ ejerSem2/ejerSem2/ejerSem2.hs
@@ -55,12 +55,17 @@ prueba5 = evalreto (Let "x" (Suma (Num 3) (Let "y" (Num 98) (Suma (Num 98) (Suma
 evalreto::Exp->Int->(Int,Int)
 evalreto (Num a) _ = (a, 0)
 evalreto (Var c) n = error "NOPE"
-evalreto (Suma exp1 exp2) n = (x1+x2,y1+y2+1+n) where
+evalreto (Suma exp1 exp2) n = (x1+x2,suma(Suma exp1 exp2)+n) where
 															(x1,y1) = (evalreto exp1 0)
 															(x2,y2) = (evalreto exp2 0) 
-evalreto (Let varSust exp1 exp2) n = (x3,y3) --evalreto (sust exp2 varSust exp1) (n)
-																		where (x1,y1) = evalreto exp1 0
-																		      (x3,y3) = evalreto (sust exp2 varSust exp1) 0
+evalreto (Let varSust exp1 exp2) n = (x3,suma(Let varSust exp1 exp2))
+																		where (x3,y3) = evalreto (sust exp2 varSust exp1) 0
+
+suma :: Exp -> Int
+suma (Num a) = 0
+suma (Var c) = 0
+suma (Suma exp1 exp2) = suma(exp1) + suma(exp2) + 1
+suma (Let varSust exp1 exp2) = suma(exp1) + suma(exp2)
 
 
 {-PRUEBAS RETO-}
